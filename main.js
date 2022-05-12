446e850aaba20d362453bd1357b6c88151a2b919
Giulia Mussetti <giuliamussetti01@gmail.com>
2022-05-12 18:42:14 +0200

    modificato

---
+++ main.js
@@ -0,0 +1,8 @@
+db.collection("Recensioni")
+.add({
+    descrizione: "Top wow",
+    utente: "pippo",
+    voto: 3,
+    });
+
+    //è un metodo asincrono che restituisce un oggetto di tipo promise!
